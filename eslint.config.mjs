a160bf1f37dc1e748a0787ba398df151697fe986
Muhammad Salman Zufar <muhammad.zufar@toyota.co.id>
2024-12-16 09:37:14 +0700

    Configure Db

---
+++ eslint.config.mjs
@@ -0,0 +1,14 @@
+import pluginJs from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {extends: ['airbnb-base', 'prettier']},
+  {files: ["**/*.{js,mjs,cjs,ts}"]},
+  {languageOptions: { globals: globals.node }},
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+
+];
